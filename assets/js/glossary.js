20f23612f3887a04243361a17f577f9a28587587
Sandesh Shahapur <sandeshshahapur@gmail.com>
2024-11-25 08:31:29 +0530

    add documentation and making image modals appear at center at their native sizes or 80% size if it exceeds screen width

--- assets/js/glossary.js
+++ assets/js/glossary.js
@@ -1,26 +1,31 @@
-const BASE_URL = window.location.origin;
+// When a glossary term is clicked, fetch the glossary data, find the definition of the term, process the definition text (for nested glossary terms, markdown, etc.), and render a modal with the definition.
+
+const BASE_URL = window.location.origin; // "https://adeveloperhasnoname.dev"
 
 const GLOSSARY_CACHE_KEY = "glossaryCache";
 const GLOSSARY_CACHE_LAST_FETCH_KEY = "glossaryCacheLastFetch";
 
 
 async function fetchGlossaryData() {
-    // manually store last time glossary data was edited in project
-    // TODO: automate this process
-    const lastEdited = new Date("November 21, 2024 14:30:00").getTime();
+    // The last edited epoch of the glossary data. This is used to check if the glossary data has been updated since the client last fetched it. It is automatically updated when the yaml glossary data is updated by script.
+    const lastEditedEpoch = 1732436272627;
+    const lastEdited = new Date(lastEditedEpoch).getTime();
 
     const now = new Date().getTime();
     const cachedData = localStorage.getItem(GLOSSARY_CACHE_KEY);
     const lastFetch = localStorage.getItem(GLOSSARY_CACHE_LAST_FETCH_KEY);
 
+    // If the glossary data has been updated since client last fetched it, fetch it again (given env is production)
     if (
-        environment === "production" &&
+        environment === "production" && // environment is set in the baseOf.html
         cachedData &&
         lastFetch && parseInt(lastFetch) > lastEdited
     ) {
         return JSON.parse(cachedData);
     }
 
+
+    // if the glossary data has been updated since client last fetched it, fetch it again, cache it, and return it
     const response = await fetch("/data/glossary.json");
     const glossaryData = await response.json();
 
@@ -32,15 +37,17 @@ async function fetchGlossaryData() {
 
 
 function getDefinition(term, glossaryData) {
+    // glossaryData is an object with term as key (lowercase) and definition as value
     return glossaryData[term.toLowerCase()];
 }
 
 
 function processGlossaryDefinitionText(tree_id, tree_node_id, definition, glossaryData) {
-    let child_num = -1;
+    let child_num = -1; // to keep track of the child number of the glossary term
 
+    // Matches glossary terms and captures the term (with optional quotes) and display term (optional).
     const glossaryPattern = new RegExp(
-        '\\{\\{\\s*<\\s*glossary term="?([^"\\s]+)"?(?:\\s+displayTerm="?((?:(?!"?\\s+>\\s*\\}\\}).)+)"?)?\\s*>\\s*\\}\\}',
+        '\\{\\{\\s*?<\\s*?glossary term="?([^"\\s]+?)"?(?:\\s+?displayTerm="?((?:(?!"?\\s+?>\\s*?\\}\\}).)+)"?)?\\s*?>\\s*?\\}\\}',
         "g"
     );
     definition = definition.replace(glossaryPattern, (_, term, displayTerm) => {
@@ -49,27 +56,38 @@ function processGlossaryDefinitionText(tree_id, tree_node_id, definition, glossa
         const displayText = (displayTerm || term).trim();
 
         // modal has to receive tree_id and tree_node_id through glossary-term
-        return termDefinition
-            ? `<span class="glossary-term" glossary-data-tree-id="${tree_id}" glossary-data-tree-node-id="${tree_node_id},${child_num}" glossary-data-term="${term}" onclick="fetchAndRenderGlossaryDefinition(this)">${displayText}</span>`.trim()
-            : displayText;
+        return `
+            <span
+                class="glossary-term" glossary-data-tree-id="${tree_id}"
+                glossary-data-tree-node-id="${tree_node_id},${child_num}"
+                glossary-data-term="${term}
+                onclick="fetchAndRenderGlossaryDefinition(this)
+            ">
+                ${displayText}
+            </span>
+        `.trim()
     });
 
-    const refPattern = new RegExp('\\[([^\\]]+)\\]\\(\\s*\\{\\{\\s*%\\s*ref\\s+\\"?([^\\"]+)\\"?\\s*%\\s*}}\\s*\\)', "g");
+    // Matches markdown ref links (with optional quotes) and replaces them with anchor tags. Note however if ref link contains whitespaces (even if within quotes), it won't be matched.
+    const refPattern = new RegExp('\\[([^\\]]+?)\\]\\(\\s*?\\{\\{\\s*?%\\s*?ref\\s+?\\"?([^\\"\\s]+?)\\"?\\s*?%\\s*?}}\\s*?\\)', "g");
     definition = definition.replace(refPattern, (_, text, absUrl) => {
         const displayText = text || absUrl;
+
+        // making link url friendly because hugo removes parentheses from file names in urls.
+        // E.g. What-Is-OAuth-(2_0)-Definition-Use-Cases-and-How-it-Works -> BASE_URL/What-Is-OAuth-2_0-Definition-Use-Cases-and-How-it-Works
         absUrl = absUrl.split("").filter(char => !["(", ")"].includes(char)).join("");
 
         if (!absUrl.startsWith("/")) absUrl = "/" + absUrl;
         return `<a href="${BASE_URL+absUrl}" target="_blank" rel="noopener">${displayText}</a>`;
     });
 
-    const boldPattern = new RegExp("\\*\\*([^**]+)\\*\\*", "g");
+    const boldPattern = new RegExp("\\*\\*([^**]+?)\\*\\*", "g");
     definition = definition.replace(boldPattern, "<strong>$1</strong>");
 
-    const hrPattern = new RegExp('\\{\\{\\s*<\\s*hr\\s*>\\s*\\}\\}')
+    const hrPattern = new RegExp('\\{\\{\\s*?<\\s*?hr\\s*?>\\s*?\\}\\}')
     definition = definition.replace(hrPattern, '<br><svg height="2" width="100%"><line x1="0" y1="0" x2="100%" y2="0" style="stroke:#ccc;stroke-width:2"/></svg><br>');
 
-    const hrSpanPattern = new RegExp('\\{\\{\\s*<\\s*hr-span\\s*>\\s*\\}\\}')
+    const hrSpanPattern = new RegExp('\\{\\{\\s*?<\\s*?hr-span\\s*?>\\s*?\\}\\}')
     definition = definition.replace(hrSpanPattern, '<span style="display: block; width: 100%; height: 1px; background-color: rgb(30, 30, 30); margin: 0.5rem 0;"></span>');
 
     return definition;
@@ -77,6 +95,7 @@ function processGlossaryDefinitionText(tree_id, tree_node_id, definition, glossa
 
 
 function isModalOpen(tree_id, tree_node_id) {
+    // fetches the tree nodes of `tree_id` and checks if any of them has the `tree_node_id`
     const tree_nodes = document.querySelectorAll(`[m-glossary-data-tree-id="${tree_id}"]`);
     return Array.from(tree_nodes).some(
         node => node.getAttribute("m-glossary-data-tree-node-id") === tree_node_id
@@ -87,13 +106,14 @@ function isModalOpen(tree_id, tree_node_id) {
 function prepareModal(modal, term, isTermImage, tree_id, tree_node_id, processedDefinition) {
     modal.classList.add("glossary-modal");
     modal.setAttribute("style", `max-width: ${ isTermImage? "100vw" : "300px"}`);
-    // Add tree_id and tree_node_id to the modal to close all child modals
+
+    // Add tree_id and tree_node_id to the modal to be able to close all child modals
     modal.setAttribute("m-glossary-data-tree-id", tree_id);
     modal.setAttribute("m-glossary-data-tree-node-id", tree_node_id);
 
     modal.innerHTML = `
         <div class="glossary-modal-header">
-            <h3 class="glossary-modal-title">${term}</h3>
+            <h5 class="glossary-modal-title">${term}</h5>
             <div class="glossary-modal-close" onclick="closeGlossaryModal(this)">&times;</div>
         </div>
         <p class="glossary-modal-content">${processedDefinition}</p>
@@ -101,20 +121,29 @@ function prepareModal(modal, term, isTermImage, tree_id, tree_node_id, processed
 }
 
 
-function positionModal(isTermImage, modal, element) {
-    const rect = element.getBoundingClientRect();
-    const modalWidth = 300; // isTermImage ? window.innerWidth : 300; // TODO or use modal.offsetWidth after showing
-    const rightSpace = window.innerWidth - rect.right;
-    const bottomSpace = window.innerHeight - rect.bottom;
+function positionModal(element, isTermImage, definition, modal) {
+    const rect = element.getBoundingClientRect(); // get the position of the element
+     // isTermImage ? window.innerWidth : 300; // TODO or use modal.offsetWidth after showing
+    const modalWidth = isTermImage ? window.innerWidth : 300; // for term, width of the modal is less than the smallest screen width we want to support (320px)
+    let topPosition = rect.bottom + window.scrollY; // position the modal below the element
 
-    let topPosition = rect.bottom + window.scrollY;
-    let leftPosition = rect.left + window.scrollX;
+    let leftPosition;
+    if (isTermImage) {
+        // set modal width to min of image width and window width or if width is not specified, set it to 80% of window width. and center it.
+        const widthPattern = /width="(\d+?)px"/;
+        const widthMatch = definition.match(widthPattern);
+        const modalWidth = widthMatch ? Math.min(parseInt(widthMatch[1]), window.innerWidth) : window.innerWidth * 0.8;
+        leftPosition = (window.innerWidth - modalWidth) / 2;
 
-    if (rightSpace < modalWidth) {
-        leftPosition = window.innerWidth - modalWidth - 20;
+        // TODO handle aspect ratio if width is altered. object-fit sucks, tried regexing definition with new calculated height but it's not working.
+    } else {
+        const rightSpace = window.innerWidth - rect.right; // space available to the right of the element
+        leftPosition = rect.left + window.scrollX; // position the modal to the right of the element
+            // If there is not enough space to the right of the element for the whole modal, try to position the modal with a right margin of 20px from the right edge of the window but if that makes the modal go off the left edge of the window, position it at left edge of the window
+            if (rightSpace < modalWidth) {
+                leftPosition = Math.max(0, window.innerWidth - modalWidth - 20);
+            }
     }
-
-    leftPosition = Math.max(0, Math.min(leftPosition, window.innerWidth - modalWidth));
 
     modal.style.top = `${topPosition}px`;
     modal.style.left = `${leftPosition}px`;
@@ -140,12 +169,12 @@ async function fetchAndRenderGlossaryDefinition(element) {
     }
 
     // Process the definition text for nested glossary terms
-    const processedDefinition = processGlossaryDefinitionText(tree_id, tree_node_id, definition, glossaryData);
+    let processedDefinition = processGlossaryDefinitionText(tree_id, tree_node_id, definition, glossaryData);
 
     // Create the modal
     const modal = document.createElement("div");
     prepareModal(modal, term, isTermImage, tree_id, tree_node_id, processedDefinition);
-    positionModal(isTermImage, modal, element);
+    processedDefinition = positionModal(element, isTermImage, processedDefinition, modal); // i know, unclean but it's best way to handle height change
 
     document.body.appendChild(modal);
 }
@@ -154,10 +183,11 @@ async function fetchAndRenderGlossaryDefinition(element) {
 function getChildrenGlossaryModals(modal) {
     let childModals = []; // Array to store child modals (including the modal itself)
 
+    // get all the tree nodes with the same tree_id as the modal
     const tree_id = modal.getAttribute("m-glossary-data-tree-id");
     const tree_nodes = document.querySelectorAll(`[m-glossary-data-tree-id="${tree_id}"]`);
 
-
+    // the child modals are the ones whose tree_node_id starts with the modal's tree_node_id
     const tree_node_id = modal.getAttribute("m-glossary-data-tree-node-id");
     childModals = Array.from(tree_nodes).filter(
         node => node.getAttribute("m-glossary-data-tree-node-id").startsWith(tree_node_id)
@@ -169,6 +199,7 @@ function getChildrenGlossaryModals(modal) {
 
 
 function closeGlossaryModal(modal) {
+    // If the close button is clicked, get the parent modal
     if (modal.classList.contains("glossary-modal-close")) {
         modal = modal.closest(".glossary-modal");
     }
@@ -181,19 +212,18 @@ function closeGlossaryModal(modal) {
 // Handle outside clicks to close modals
 setTimeout(() => {
     window.addEventListener("click", function closeOnOutsideClick(event) {
-        var glossary_terms = document.getElementsByClassName("glossary-term");
-        var modals = document.getElementsByClassName("glossary-modal");
-
-
         // Do not close any modal if a glossary term is clicked
+        var glossary_terms = document.getElementsByClassName("glossary-term");
         for (var j = 0; j < glossary_terms.length; j++) if (event.target === glossary_terms[j]) return;
 
         // Do not close any modal if a modal is clicked
+        var modals = document.getElementsByClassName("glossary-modal");
         for (var i = 0; i < modals.length; i++) if (modals[i].contains(event.target)) return;
-        // Handling bug or something that closes all modals if clicked on a modal's close button
-        if (event.target.classList.contains("glossary-modal-close")) return;
 
-        // Close all modals if clicked outside of any modal
+        // Handling bug or something that closes all modals if clicked on a modal's close button
+        if (event.target.classList.contains("glossary-modal-close")) return; // closGlossaryModal will handle this
+
+        // As no glossary term or modal was clicked, close all modals
         Array.from(modals).forEach(modal => modal.remove());
     });
 }, 0);
